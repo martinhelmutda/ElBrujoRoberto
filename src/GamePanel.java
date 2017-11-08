a39ad997766dd4a396607106f1e652d55d3efa5c
Unknown <martinhelmutmh@gmail.com>
2017-11-07 18:13:27 -0600

    Buffered Image
    Ya corren las imágenes pero hace falta que corran en ciertos momentos

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,19 +2,22 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements Runnable, KeyListener {
+public class GamePanel extends JPanel implements Runnable, KeyListener, ActionListener,ImagesPlayerWatcher {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	static final int VWIDTH = 800;
 	static final int VHEIGHT= 600;
@@ -25,9 +28,25 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	private int FPS=60;
 	private long tiempoObj= 1000/FPS;
 	
+	private final static String IMS_FILE = "imsInfo.txt";
+	private static final int PERIOD = 1000;
+	 //-------------
+	  private ImageLoader imsLoader;   // the image loader
+	  private int counter;
+	  private boolean justStarted;
+	  
+	  private GraphicsDevice gd;   // for reporting accl. memory usage
+	  private int accelMemory;   
+	  private DecimalFormat df;
+	  
+	  
+	  private ImagesPlayer numbersPlayer;
+	  
+	  
+	//------------------
+	
 	private GameStateContext contexto;
 	
-	private ImagesPlayer numbersPlayer;
 	
 	public GamePanel() {
 		setPreferredSize(new Dimension(VWIDTH,VHEIGHT));
@@ -41,12 +60,109 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 		});
 		
 		contexto = new GameStateContext();
+		df = new DecimalFormat("0.0");  // 1 dp
+
+	    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	    gd = ge.getDefaultScreenDevice();
+
+	    accelMemory = gd.getAvailableAcceleratedMemory();  // in bytes
+	    System.out.println("Initial Acc. Mem.: " + 
+	                   df.format( ((double)accelMemory)/(1024*1024) ) + " MB" );
+
+	    imsLoader = new ImageLoader(IMS_FILE); 
+	    
+	    initImages();
+
+	    counter = 0;
+	    justStarted = true;
+
+	    new Timer(PERIOD, this).start();  
 	}
+	
+	private void initImages()
+	  {
+	    // initialize the 'o' image variables
+
+
+	    /* Initialize ImagesPlayers for the 'n' and 's' images.
+	       The 'numbers' sequence is not cycled, the other are. 
+	    */
+	    numbersPlayer = 
+	         new ImagesPlayer("numbers", PERIOD, 6 , false, imsLoader);
+	    numbersPlayer.setWatcher(this);     // report the sequence's end back here
+
+
+	  }
+	
+	public void sequenceEnded(String imageName)
+	  // called by ImagesPlayer when its images sequence has finished
+	  {  System.out.println( imageName + " sequence has ended");  }
+
+	 public void actionPerformed(ActionEvent e)
+	  // triggered by the timer: update, repaint
+	  { 
+	    if (justStarted)   // don't do updates the first time through
+	      justStarted = false;
+	    else
+	      imagesUpdate();
+
+	    repaint();  
+	  }
+	
+	  private void imagesUpdate()
+	  { 
+	    // numbered images ('n' images); using ImagesPlayer
+	    numbersPlayer.updateTick();
+	    if (counter%30 == 0)     // restart the image sequence periodically
+	      numbersPlayer.restartAt(0);//número de inicio
+
+	 
+
+	    // grouped images ('g' images)
+	    // The 'fighter' images are the only grouped images in this example.
+	  
+	  } 
+	
+	 
+	
+	  
+	  
+	  private void drawImage(Graphics2D g2d, BufferedImage im, int x, int y)
+	   /* Draw the image, or a yellow box with ?? in it if there is no image. */
+	   { 
+	     if (im == null) {
+	       // System.out.println("Null image supplied");
+	       g2d.setColor(Color.yellow);
+	       g2d.fillRect(x, y, 20, 20);
+	       g2d.setColor(Color.black);
+	       g2d.drawString("??", x+10, y+10);
+	     }
+	     else
+	       g2d.drawImage(im, x, y, this);
+	   } // end of drawImage()
+
+	  private void reportAccelMemory()
+	  // report any change in the amount of accelerated memory
+	  {
+	    int mem = gd.getAvailableAcceleratedMemory();   // in bytes
+	    int memChange = mem - accelMemory;
+
+	    if (memChange != 0) 
+	      System.out.println(counter + ". Acc. Mem: " + 
+	                df.format( ((double)accelMemory)/(1024*1024) ) + " MB; Change: " +
+	                df.format( ((double)memChange)/1024 ) + " K");
+	    accelMemory = mem;
+	  }  // end of reportAcceleMemory()
+//--------------------------PARTE DE CODIGO
+	
 	
 	public void addNotify(){
 		super.addNotify();
 		start();
 	}
+	
+	
+	
 	
 	public void start() {
 		running=true;
@@ -81,6 +197,11 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 		 
 	}
 
+	
+	
+	
+	
+	
 
 
 	private void tick() {
@@ -94,8 +215,11 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	public void paintComponent(Graphics dbg) {
 		super.paintComponent(dbg);
 		Graphics2D g2d = (Graphics2D)dbg; 
-		dbg.clearRect(0, 0, VWIDTH, VHEIGHT);
 		contexto.paint(dbg); 	//Pintamos las im�genes que el director administra
+		
+		reportAccelMemory();
+		drawImage(g2d, numbersPlayer.getCurrentImage(), VWIDTH/2, VHEIGHT/2);
+		counter = (counter + 1)% 100;   
 	}
 
 	
